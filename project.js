f0dc3271b3ea1decdbf8abcdb7e4136639e12a83
Suraj Rahel <suraj.rahel@dpdgroup.co.uk>
2015-12-04 15:40:46 +0000

    added api

--- project.js
+++ project.js
@@ -1,4 +1,4 @@
 var app = angular.module("myApp",[]);
-app.controller('SettingsController1',function($scope){
+app.controller('contactManagerCtrl',function($scope){
 	$scope.yourName = "DPD App";
 });
